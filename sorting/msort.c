28f8303cdb90e8d3f6c389a52993c2f6aa4200ef
Manu <manukumar4648@gmail.com>
2025-04-22 13:07:12 +0530

    test

---
+++ sorting/msort.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdio.h>
+
+void main(void){
+    int arr[10] = {12, 53, 54, 13, 64, 43, 95, 38, 9, 10};
+    for(int i = 0; (i < sizeof(arr)/sizeof(int))/2; i++){
+        
+    }
+    for(int j = sizeof(arr)/sizeof(int); j < sizeof(arr)/sizeof(int) -1; j++){}
+}
